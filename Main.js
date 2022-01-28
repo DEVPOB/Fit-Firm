83e46ea7685b4a5b9bf695139e4993b17fdcaa93
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-01-28 14:30:11 +0700

    .
    .

--- Main.js
+++ Main.js
@@ -1,13 +1,44 @@
 
 function changetest(){
+    const navliHome = document.getElementById('activeHome')
+    const navliInfo = document.getElementById('activeInfo')
+    const navlidemo = document.getElementById('activeDemo')
+    const navliaboutme = document.getElementById('activeAboutme')
     var nav = document.getElementById("navbar");
-    if (window.scrollY > 100) {
+    // cring javascript
+    if (window.scrollY > 170) {
         nav.classList.add('bg');
     }
     else{
         nav.classList.remove('bg');
     }
-    
-}
+    if (window.scrollY >= 0 && window.scrollY <= 500) {
+        navliHome.classList.add('activeHomeclass');
+    }
+    else{
+        navliHome.classList.remove('activeHomeclass');
+    }
 
+    if (window.scrollY >= 500 && window.scrollY <= 2128) {
+        navliInfo.classList.add('activeInfoclass');
+    }
+    else{
+        navliInfo.classList.remove('activeInfoclass');
+    }
+
+    if (window.scrollY >= 2129 && window.scrollY <= 3000) {
+        navlidemo.classList.add('activeDemoclass');
+    }
+    else{
+        navlidemo.classList.remove('activeDemoclass');
+    }
+
+    if (window.scrollY >= 3000) {
+        navliaboutme.classList.add('activeAboutmeclass');
+    }
+    else{
+        navliaboutme.classList.remove('activeAboutmeclass');
+    }
+}
 window.addEventListener('scroll', changetest)
+
